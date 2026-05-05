d1922fb5baee893208668828ac4e6d2c18f226e5
Alex Iancu <alexiancu1306@gmail.com>
2026-05-05 14:03:41 +0200

    feature/ Endpoint master password

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from core.views import verify_master_password
+
 urlpatterns = [
     path("", RedirectView.as_view(url='/admin/', permanent=False)),
     path("admin/", admin.site.urls),
@@ -13,5 +15,6 @@ urlpatterns = [
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/users/", include("users.api.urls")),
     path("api/games/", include("games.api.urls")),
-    path("api/chat/", include("chat.api.urls"))
+    path("api/chat/", include("chat.api.urls")),
+    path("api/core/verify-master-password/", verify_master_password, name="verify-master-password")
 ]
