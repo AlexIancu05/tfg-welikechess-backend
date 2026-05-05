4446ccac704a6a508392f4ba57464cebbf085d00
Alex Iancu <alexiancu1306@gmail.com>
2026-05-05 14:12:02 +0200

    fix/ Endpoint master password

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view
+@api_view(["POST"])
 @permission_classes([AllowAny])
 def verify_master_password(request):
     """
