85d72858187d88c364dac2cb5604f7eecf5a7f03
Alex <alexiancu1306@gmail.com>
2026-05-20 17:28:13 +0000

    fix/ Mayor paginacion drf

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ AUTH_USER_MODEL = "users.User"
 # Django Rest Framework
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 100,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
