a98f213a576b55fa1bfae8cc86b3f975d42e70f8
Alex <alexiancu1306@gmail.com>
2026-05-20 21:12:27 +0000

    fix/ Registro ahora devuelve token

--- users/api/views.py
+++ users/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, viewsets, filters, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from django.shortcuts import get_object_or_404
 
@@ -70,6 +71,21 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                **serializer.data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_201_CREATED,
+        )
 
     @action(detail=False, methods=["get"], url_path="leaderboard")
     def leaderboard(self, request):
